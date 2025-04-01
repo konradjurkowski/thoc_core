e210bb954ff2a828697394b1a0e4f71c55c9a69b
konradjurkowski <konrad.j@thehouseofcode.com>
2025-04-01 19:23:52 +0200

    correct base controller

--- lib/architecture/base_controller.dart
+++ lib/architecture/base_controller.dart
@@ -1,26 +1,34 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class BaseController<S, I, E> extends AutoDisposeNotifier<S> {
-  final StateProvider<E?> eventProvider;
 
-  BaseController(this.eventProvider);
-
+  final _eventNotifierProvider = NotifierProvider<EventNotifier<E>, E?>(() => EventNotifier<E>());
+  NotifierProvider<EventNotifier<E>, E?> get eventProvider => _eventNotifierProvider;
+  
   S buildState();
   void processIntent(I intent);
 
   void sendEvent(E event) {
-    ref.read(eventProvider.notifier).state = event;
+    ref.read(eventProvider.notifier).emit(event);
   }
 
   @override
   S build() {
-    ref.onDispose(() => ref.read(eventProvider.notifier).state = null);
+    ref.onDispose(() => ref.read(eventProvider.notifier).reset());
     return buildState();
   }
 }
 
-extension ProviderListenableExt<T> on ProviderListenable<T> {
-  void observe(WidgetRef ref, void Function(T) onEvent) {
-    ref.listen(this, (_, event) => onEvent(event));
+class EventNotifier<E> extends Notifier<E?> {
+  @override
+  E? build() => null;
+
+  void emit(E event) {
+    state = null;
+    state = event;
+  }
+
+  void reset() {
+    state = null;
   }
 }
